b266d512fd17b21e154915841934c60d4a900784
Marek Jaczewski <jaczewski147@gmail.com>
2019-04-21 17:11:32 +0200

    sketch of methods

--- src/com/mj/Game.java
+++ src/com/mj/Game.java
@@ -1,4 +1,37 @@
 package com.mj;
 
+import java.util.ArrayList;
+
 public class Game {
+
+    InputField input = new InputField();
+    ArrayList<Battleship> battleshipList = new ArrayList<>();
+    private int numberOfMoves = 0;
+
+    public int getNumberOfMoves() {
+        return numberOfMoves;
+    }
+
+    public void setNumberOfMoves(int numberOfMoves) {
+        this.numberOfMoves = numberOfMoves;
+    }
+
+    public void prepareGame() {
+        //create, set name, set position of objects battleship
+        //display information for player
+    }
+
+    public void startGame() {
+        //ask player for move
+        //initial checkMove
+    }
+
+    public void checkMove() {
+        //initial check()
+    }
+
+    public void endGame() {
+        //print number of moves
+    }
+
 }
