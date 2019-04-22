654cb4538119219639c58da8220833f78d8b7d17
Marek Jaczewski <jaczewski147@gmail.com>
2019-04-22 22:29:27 +0200

    add get methods, change fields name, add description

--- src/com/mj/InputField.java
+++ src/com/mj/InputField.java
@@ -7,9 +7,22 @@ import java.util.ArrayList;
 
 public class InputField {
 
-    private static final String alfabet = "abcdefg";
+    private final String abc = "abcdefg";
     private int lengthOfBoard = 7;
     private int sizeOfBoard = 49;
+
+    public String getABC() {
+        return abc;
+    }
+
+    public int getLengthOfBoard() {
+        return lengthOfBoard;
+    }
+
+    public int getSizeOfBoard() {
+        return sizeOfBoard;
+    }
+
     private int[] board = new int[sizeOfBoard];
     private int numberOfBattleships = 0;
 
@@ -31,17 +44,18 @@ public class InputField {
 
     public ArrayList setupBattleship (int sizeOfBattleship) {
         ArrayList<String> occupiedFields = new ArrayList<>();
-        String help = null;
+        String convertToABC = null;
         int [] coordinate = new int[sizeOfBattleship];
-        int prob = 0;
+        int numberOfTries = 0;
         boolean success = false;
         int position = 0;
         numberOfBattleships++;
-        int inkr = 1;
+        int increase = 1;
+
         if ((numberOfBattleships % 2) == 1) { // if odd -> set vertical
-            inkr = lengthOfBoard; // vertical increase
+            increase = lengthOfBoard; // vertical increase
         }
-        while (!success & prob++ < 200 ) {
+        while (!success & numberOfTries++ < 200 ) {
             position = (int) (Math.random() * sizeOfBoard); // random start point
             int x = 0; //
             success = true;
@@ -49,11 +63,11 @@ public class InputField {
             while (success && x < sizeOfBattleship) {
                 if (board[position] == 0) {
                     coordinate[x++] = position;
-                    position += inkr;
-                    if (position >= sizeOfBoard) {
+                    position += increase;
+                    if (position >= sizeOfBoard) { // out of bottom edge
                         success = false;
                     }
-                    if (x > 0 & (position % lengthOfBoard == 0)) {
+                    if (x > 0 && (position % lengthOfBoard == 0)) { // out of right edge
                         success = false;
                     }
                 } else { // field is occupied
@@ -61,18 +75,19 @@ public class InputField {
                 }
             }
         }
+
         int x = 0;
-        int wiersz = 0;
-        int kolumna = 0;
+        int line = 0;
+        int column = 0;
 
         while (x < sizeOfBattleship) {
             board[coordinate[x]] = 1; // mark occupied field
-            wiersz = (int) (coordinate[x] / lengthOfBoard); // which line
-            kolumna = coordinate[x] % lengthOfBoard; // get number of column
-            help = String.valueOf(alfabet.charAt(kolumna)); // convert to alphanumeric
-            occupiedFields.add(help.concat(Integer.toString(wiersz)));
+            line = (int) (coordinate[x] / lengthOfBoard); // which line
+            column = coordinate[x] % lengthOfBoard; // get number of column
+            convertToABC = String.valueOf(abc.charAt(column)); // convert to alphanumeric
+            occupiedFields.add(convertToABC.concat(Integer.toString(line)));
             x++;
-            //System.out.print(" współrzędne " + x + " = " + occupiedFields.get(x-1));
+            //System.out.print(" współrzędne " + x + " = " + occupiedFields.get(x-1)); // print coordinates of Battleships
         }
 
         return occupiedFields;
