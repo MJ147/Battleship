8b7b4e3414bb542e2f9b2aa1ef0c7f2caec39ec4
Marek Jaczewski <jaczewski147@gmail.com>
2019-04-22 22:40:10 +0200

    test implementation for Battleship class, check() method

--- src/com/mj/BattleshipTest.java
+++ src/com/mj/BattleshipTest.java
@@ -1,4 +1,34 @@
 package com.mj;
 
+import java.util.ArrayList;
+
 public class BattleshipTest {
+
+    public static void main(String[] args) {
+
+        Battleship battleship = new Battleship("Name");
+        ArrayList<String> position = new ArrayList<>();
+
+        position.add("A1");
+        position.add("A2");
+        position.add("A3");
+
+        battleship.setPosition(position);
+
+        String move = "A1";
+
+        String result = battleship.check(move);
+
+        String resultOfTest = "megative";
+
+        if (result.equals("trafiony")) {
+            resultOfTest = "positive";
+        }
+
+        System.out.println(resultOfTest);
+
+
+
+
+    }
 }
